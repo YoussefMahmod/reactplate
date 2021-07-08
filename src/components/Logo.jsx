afcd4a9df3463dda8ad4c1f0e394bb978bd893aa
Muhammad Fauzan <50759463+fauzan121002@users.noreply.github.com>
2021-07-08 12:14:04 +0700

    update: revert logo changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo() {
     <>
       {/* Imported images have the side benefit that, if the file is missing, the
       build will fail, and you’ll find out quick */}
-      <h1 class="logo"><span>React</span>plate</h1>
+      <img src={ReactplateLogo} width="400" height="400" alt="" />
     </>
   );
 }
